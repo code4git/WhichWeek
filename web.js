746d6452b8a9d07cdc448c419b8b1c02d5c1f327
d-o-k <dimitri.okunev@gmail.com>
2014-12-20 18:45:33 +0200

    Revert "add profile for heroic"
    This reverts commit a9c617f89794e69737e7a971b8b5db200923db47.

--- web.js
+++
@@ -1,7 +0,0 @@
-var gzippo = require('gzippo');
-var express = require('express');
-var app = express();
-
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(pcoess.end.PORT || 5000);s
